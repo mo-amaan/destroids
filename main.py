96b90fea354c686c9f74b032f34506d22b355a22
Mo Amaan <mo.amaan215@gmail.com>
2025-09-01 22:31:06 -0300

    player movement

--- main.py
+++ main.py
@@ -22,10 +22,13 @@ def main():
 
         dt = clock.tick(60)/1000
 
+        #here is where the player values are being updated every frame
+        player.update(dt)
         player.draw(screen)
 
 
         pygame.display.flip()
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
