8a750648f03f0959a13f6388a06ed6a0786a21c3
Mohammed Amaan <mo.amaan215@gmail.com>
2025-09-12 22:45:50 -0300

    Changes to main

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable,drawable)
-    Asteroids.containers = (asteroids,updatable,drawable)
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = (updatable,)
 
     player = Player(SCREEN_WIDTH/2 , SCREEN_HEIGHT/2)
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
